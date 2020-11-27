9a4f143aa81b0bb2764fd64281c427403d43c61d
Lamine AMIMI <alamine@nordnet.net>
2020-11-27 13:55:59 +0100

    update product service with methods

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 
+import { Product } from '../model/product';
+import { PRODUCTS } from '../model/PRODUCT_Mock';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  produts = PRODUCTS;
+
+  getProduct(): Product[] {
+    return PRODUCTS;
+  }
+  isTheLast(product:Product){
+    return product.stock ===1;
+  }
+  isAvailable(product: Product) {
+    return product.stock !== 0;
+
+  }
+  decreaseStock(product: Product){
+    product.stock = product.stock -1;
+
+  }
 
   constructor() { }
 }

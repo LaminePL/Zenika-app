3e8c815239ff3d197d04b59b61877abf8e4fd553
Lamine AMIMI <alamine@nordnet.net>
2020-11-27 11:49:42 +0100

    add Angular Material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProductComponentComponent } from './product-component/product-component.component';
 import { BorderCardDirective } from './border-card.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BorderCardDirective } from './border-card.directive';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

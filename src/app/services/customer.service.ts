7b00faae547372f7757afc4ee150388a0f50824a
Lamine AMIMI <alamine@nordnet.net>
2020-11-27 14:25:38 +0100

    Customer service methods

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { Product } from '../model/product';
+import { PRODUCTS } from '../model/PRODUCT_Mock';
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
+  pannier: Array<Product>;
+  montant = 0;
+  addProduct( product:Product){
+  this.pannier.push(product);
+  }
+
+  getTotal(){
+    this.montant = this.pannier.map(product => product.price).reduce((prev, curr) => prev + curr, 0)
+// je recupere mon pannier avec avec les produits.
+// avec la fonction .map() je recupere le product price de product
+// reduce() recupere tous les price et fait prev + curr a chaque fois pour recuperer le total
+  }
   constructor() { }
 }

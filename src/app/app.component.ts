607f1ca1e57a4f4149cd93172645ea83f28bb2f5
Lamine AMIMI <alamine@nordnet.net>
2020-12-11 12:04:21 +0100

    FilterPipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { provideRoutes } from '@angular/router';
 import { Product } from './model/product';
 import { PRODUCTS } from './model/PRODUCT_Mock';
 
-import{CustomerService} from './services/customer.service';
-import{ProductService} from './services/product.service';
+import { CustomerService } from './services/customer.service';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +12,25 @@ import{ProductService} from './services/product.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
+
   title = 'Zenika';
-  products: Product[] =[];
+  products: Product[] = [];
   total: number = 0;
   stock: number = 0;
   baskets: Product[] = [];
-  
- constructor(private productService: ProductService ,  private customerService: CustomerService) {
-   
+
+  constructor(private productService: ProductService, private customerService: CustomerService) {
   }
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(products =>  this.products = products)
+    this.productService.getProducts().subscribe(products => this.products = products)
     this.customerService.getBasket().subscribe(products => this.baskets = products)
-  
   }
- 
-
-  addProduct(product:Product){
-    this.customerService.addProduct(product).subscribe(res => { console.log(res)})
+  addProduct(product: Product) {
+    this.customerService.addProduct(product).subscribe(res => { console.log(res) })
   }
-  displayPrice(price: number) {
+   displayPrice(price: number) {
     this.total += price;
   }
-  
-
-
 }
 
 

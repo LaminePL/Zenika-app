27a1171b2085c2b9f8910dd3e45b2123b74979e6
Lamine AMIMI <alamine@nordnet.net>
2020-12-11 14:35:15 +0100

    reactive form

--- src/app/customerform/customerform.component.ts
+++ src/app/customerform/customerform.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormArray } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Customer } from '../model/customer';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
   selector: 'app-customerform',
@@ -10,26 +12,19 @@ import { Customer } from '../model/customer';
   styleUrls: ['./customerform.component.css']
 })
 export class CustomerformComponent implements OnInit {
-      customer: Customer;
+  customer: Customer;
 
   profileForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('',  Validators.required),
     adress: new FormControl(''),
-    creditCard: new FormControl(''),
+    creditCard: new FormControl('', Validators.pattern('^[0-9]{3}-[0-9]{3}$') ),
   });
-
-
-
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private customerService: CustomerService, private router: Router) { }
   ngOnInit(): void {
   }
-
-
-
-
   onSubmit() {
-    
-    // TODO: Use EventEmitter with form value
-    console.log(Object.values(this.profileForm.value));
+
+    this.customerService.checkout({ name: this.profileForm.get('name').value, adress: this.profileForm.get('adress').value, creditCardType: this.profileForm.get('creditCard').value }).subscribe(() => this.router.navigate(['']));
+
   }
 }

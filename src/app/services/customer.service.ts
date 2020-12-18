d8b89c30c403652fe80c220074ab89864fcfab91
Lamine AMIMI <alamine@nordnet.net>
2020-12-18 15:33:56 +0100

    customer service clean

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,13 +1,10 @@
-import { Injectable, PLATFORM_INITIALIZER } from '@angular/core';
-import { Product } from '../model/product';
-import { PRODUCTS } from '../model/PRODUCT_Mock'
-import { ProductService } from './product.service';
-import { ProductComponentComponent } from '../product-component/product-component.component';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { from, Observable, of, throwError } from 'rxjs';
-import { catchError, retry, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Customer } from '../model/customer';
+import { Product } from '../model/product';
 @Injectable({
   providedIn: 'root'
 })
@@ -17,23 +14,23 @@ export class CustomerService {
   montant = 0;
 
   getTotal() {
-    this.montant = this.pannier.map(product => product.price).reduce((prev, curr) => prev + curr, 0)
+    this.montant = this.pannier.map(product => product.price).reduce((prev, curr) => prev + curr, 0);
   }
 
   private handleError<T>(operation = 'operation', results?: T) {
     return (error: any): Observable<T> => {
       console.log(error);
       console.log(`${operation} failed: ${error.message}`);
-      return of(results as T)
+      return of(results as T);
 
     };
   }
   private log(log: string) {
-    console.info(log)
+    console.info(log);
 
   }
   getBasket(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url_api + '/basket')
+    return this.http.get<Product[]>(this.url_api + '/basket');
   }
   addProduct(product: Product): Observable<Product> {
     return this.http.post<Product>(this.url_api + '/basket', product).pipe(
